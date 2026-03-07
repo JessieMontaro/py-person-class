6b76624b53e1fc727871c5534eb33fab50fc852c
Jessie Montaro <montaro.daedra@gmail.com>
2026-03-07 15:37:39 +0100

    Completed the task, sending first instance

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        self.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    if people == []:
+        return []
+    people_class_list = []
+    for person in people:
+        people_class_list.append(Person(person["name"], person["age"]))
+    for i in range(len(people)):
+        if "wife" in people[i] and people[i]["wife"] is not None:
+            people_class_list[i].wife = Person.people[people[i]["wife"]]
+        elif "husband" in people[i] and people[i]["husband"] is not None:
+            people_class_list[i].husband = Person.people[people[i]["husband"]]
+    return people_class_list
