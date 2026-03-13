0a763a05b3d61fcdc5efb529389618989b6c5a89
Jessie Montaro <montaro.daedra@gmail.com>
2026-03-13 03:50:02 +0100

    Fixed final wife-husband errors

--- app/main.py
+++ app/main.py
@@ -14,8 +14,8 @@ def create_person_list(people: list) -> list:
     for person in people:
         people_class_list.append(Person(person["name"], person["age"]))
     for i in range(len(people)):
-        if "wife" in people[i] and people[i]["wife"] is not None:
-            people_class_list[i].wife = Person.people[people[i]["wife"]]
-        elif "husband" in people[i] and people[i]["husband"] is not None:
-            people_class_list[i].husband = Person.people[people[i]["husband"]]
+        if people[i].get("husband") is not None:
+            people_class_list[i].husband = Person.people.get(people[i].get("husband"))
+        if people[i].get("wife") is not None:
+            people_class_list[i].wife = Person.people.get(people[i].get("wife"))        
     return people_class_list
